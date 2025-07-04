02e7a891c9651a028312ece43b3b3e72bbb214e8
FabioMainato <fabiaochirindzamainato@gmail.com>
2025-07-04 11:51:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const resourcesData = [
   {
+    title: "Simplicidade",
     img: "/images/logos/simple.png",
     alt: "Simple",
     text: "Interface simples e intuitiva, desenvolvida para que qualquer pessoa possa operar o sistema com rapidez, sem necessidade de treinamento técnico.",
   },
   {
+    title: "Simplicidade",
     img: "/images/logos/manager.png",
     alt: "Manager",
     text: "Gerencie produtos com códigos de barras, quantidades, preços e imagens. Receba alertas de estoque baixo.",
   },
   {
+    title: "Simplicidade",
     img: "/images/logos/develop.png",
     alt: "Desempenho",
     text: "Acompanhe o desempenho do seu negócio com relatórios completos de vendas, produtos mais vendidos, formas de pagamento.",
